31e6e2ac64e97170923f0c832c3ffa2e27e3f78d
Parth Andhariya <parth.andhariya@jbspl.com>
2018-10-17 15:46:23 +0530

    example app

---
+++ example/EventList.js
@@ -0,0 +1,58 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  FlatList,
+} from 'react-native';
+
+import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#F3F3F3'
+  },
+});
+
+class EventList extends Component {
+  state = {
+    events: [],
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now(),
+        })),
+      });
+    }, 1000);
+
+    const events = require('./db.json').events.map(e => ({
+      ...e,
+      date: new Date(e.date),
+    }));
+    this.setState({ events });
+  }
+
+
+  render() {
+    return (
+      <FlatList
+        key="flatlist"
+        data={this.state.events}
+        style={styles.list}
+        keyExtractor={item => item.id}
+        renderItem={({ item, separators }) => (
+          <EventCard
+            event={item}
+          />
+        )}
+      />
+    );
+  }
+}
+
+export default EventList;
